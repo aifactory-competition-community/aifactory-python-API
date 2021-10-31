7ccb5d8349b1df89803e1628e6b18758bd46df46
yoosunyoung <luysunyoung9@gmail.com>
2021-10-31 22:01:59 +0900

    split submission order more general form

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(  name='aifactory_alpha',
 
-        version='1.2.5',
+        version='1.2.6',
 
         description='alpha version of AI Factory client api package',
 
